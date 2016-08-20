c3e3de830000c573a7f36690677c98a5a2296843
sveeke <mail@sebasveeke.nl>
2016-08-21 00:34:46 +0200

    Update temperature.sh

--- freebsd/temperature.sh
+++ freebsd/temperature.sh
@@ -10,11 +10,8 @@ echo
 echo "Drive temperature:"
 for i in $(sysctl -n kern.disks)
 do
-        DevTemp=`smartctl -a /dev/$i | awk '/Temperature_Celsius/{print
-$0}' | awk '{print $10 "C"}'`
-        DevSerNum=`smartctl -a /dev/$i | awk '/Serial Number:/{print
-$0}' | awk '{print $3}'`
-        DevName=`smartctl -a /dev/$i | awk '/Device Model:/{print $0}' |
-awk '{print $3}'`
-        echo $i $DevTemp $DevSerNum $DevName
+DevTemp=`smartctl -a /dev/$i | awk '/Temperature_Celsius/{print $0}' | awk '{print $10 "C"}'`
+DevSerNum=`smartctl -a /dev/$i | awk '/Serial Number:/{print $0}' | awk '{print $3}'`
+DevName=`smartctl -a /dev/$i | awk '/Device Model:/{print $0}' | awk '{print $3}'`
+echo $i $DevTemp $DevSerNum $DevName
 done
